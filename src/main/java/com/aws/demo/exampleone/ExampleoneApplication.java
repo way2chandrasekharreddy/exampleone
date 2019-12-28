370e02837a999695423c00091e12b3efa51c7fca
way2chandrasekhar <way2chandrasekhar@gmail.com>
2019-12-29 03:07:46 +0530

    first commit

---
+++ src/main/java/com/aws/demo/exampleone/ExampleoneApplication.java
@@ -0,0 +1,13 @@
+package com.aws.demo.exampleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleoneApplication.class, args);
+	}
+
+}
