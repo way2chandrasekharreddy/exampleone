18090e7e5f03e17d3df80bbb4e17fc917b8a59a6
way2chandrasekhar <way2chandrasekhar@gmail.com>
2019-12-29 04:18:00 +0530

    added yml

---
+++ src/main/java/com/aws/demo/exampleone/Controller.java
@@ -0,0 +1,15 @@
+package com.aws.demo.exampleone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+	
+	@GetMapping
+	public String hello() {
+		return "hello tech this form my code..";
+	}
+}
